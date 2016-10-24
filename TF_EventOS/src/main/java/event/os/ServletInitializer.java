04e05cb0f1d69b779e6dbaa5952444fcd8e144f3
Sergio Rios <Sergio Rios>
2016-10-24 15:42:42 -0500

    Nuevo Proyecto Final Open Source

---
+++ TF_EventOS/src/main/java/event/os/ServletInitializer.java
@@ -0,0 +1,13 @@
+package event.os;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(TfEventOsApplication.class);
+	}
+
+}
---
+++ TF_EventOS/src/main/java/event/os/TfEventOsApplication.java
@@ -0,0 +1,12 @@
+package event.os;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TfEventOsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TfEventOsApplication.class, args);
+	}
+}
